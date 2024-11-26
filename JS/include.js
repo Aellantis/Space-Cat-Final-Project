9968bda6a78669b2c652c2c638ed5161663febac
Aellantis <Ashleyyung.1231@gmail.com>
2024-11-26 00:03:58 -0800

    trying something new

---
+++ JS/include.js
@@ -0,0 +1,29 @@
+function includeHTML(file, elementId) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+      if (elementId === 'navbar') {
+        initializeNavbarScrollEffect();
+      }
+    });
+}
+
+function initializeNavbarScrollEffect() {
+  const navbar = document.getElementById('navbar');
+
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
+  }
+}
+
+window.onload = () => {
+  includeHTML('/Components/Navbar/navbar.html', 'navbar');
+  includeHTML('/Components/Footer/footer.html', 'footer');
+};
